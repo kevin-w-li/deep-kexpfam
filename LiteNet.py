1b18e6a4a301a4db8e9e513f837e3f980a7a4ce1
Kevin Li <kevinwli@outlook.com>
2017-07-14 16:22:01 +0100

    score matching part working

--- LiteNet.py
+++ LiteNet.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 class ExponentialKernel:
 
@@ -51,24 +52,128 @@ class LiteModel:
         self.ndata  =  network.ndata
         self.network = network
         self.kernel  = kernel
-        self.alpha   = None
+        self.X       = self.kernel.X
 
     def set_points(self, X):
+        '''This sets up the set of points used by model x_i's
+        Input is a set of images that will first be processed by network
+        These are stored inside the model as parameters
+        '''
         self.kernel.set_points(self.network.forward(X))
         self.ndim = self.kernel.ndim
         self.npoint = self.kernel.npoint
+        self.X      = self.kernel.X
 
-    def score(self, data):
+    def score_original(self, data):
+
+        ''' This computs the score objective without the network-dependent 
+            base measure q_0
+        '''
+
         sigma = self.kernel.sigma
+        N = self.ndata
+        alpha = self.kernel.alpha
+        ndim  = self.ndim
+
+        # output of the network, same as x's
         Y = self.network.forward(data)
-        G = self.kernel.gram_matrix(Y)
-        D = 2.0/sigma*self.kernel.pdist2 - self.ndim
-        # J = tf.reduce_sum(D) * 2 / self.ndata / sigma
-        J = tf.reduce_sum(tf.expand_dims(self.alpha,1) * G * D) * 2.0 / self.ndata / sigma
+
+        # compute gram matrix of network output with points
+        K = self.kernel.gram_matrix(Y)
+        D = 2.0/sigma*self.kernel.pdist2 - ndim
+        t = tf.reduce_sum(tf.expand_dims(alpha,1) * K * D) * 2.0 / N / sigma
+        
+        # X_il
+        # Y_jl
+        X = self.X
+        # C = (X[:,None,:] - x[None,:,:]), C_ijl
+        S = tf.expand_dims(X,1) - tf.expand_dims(Y,0)
+
+        # alpha_exp = alpha[:,None, NOne], alpha_exp_i..
+        alpha_exp = tf.expand_dims(tf.expand_dims(alpha, -1),-1)
+
+        # K_exp = K[:,:,None], K_ij.
+        K_exp     = tf.expand_dims(K, -1)
+
+        J += 2.0/N/(sigma**2)*tf.reduce_sum(
+                            tf.reduce_sum(alpha_exp * (S) * K_exp, 0
+                        )**2)
 
         return J
-            
-            
+
+    def _process_data(self, data):
+
+        if data is not None:
+                Y = self.network.forward(data)
+        else:
+            if self.X is None:
+                raise NameError('run set_point first to set the kernel points, or provide it as input')
+            Y = self.X
+        return Y
+
+    def score(self, data=None):
+        
+        Y = self._process_data(data)
+
+        # alpha as vectors
+        # alpha as vectors
+        alpha = self.kernel.alpha
+        alpha_V = tf.expand_dims(alpha,1)
+        alpha_T = tf.expand_dims(alpha,0)
+
+        sigma = self.kernel.sigma
+
+        b, C = self._lite_score_stats(Y)
+
+        J = 2.0 / self.ndata / sigma    * tf.einsum('i,i'   , alpha, b) + \
+            2.0 / self.ndata / sigma**2 * tf.einsum('i,ij,j', alpha, C, alpha)
+
+        return J
+
+    def _lite_score_stats(self, Y):
+        
+        ''' compute the vector b and matrix C
+            Y: the input data to the lite model to fit
+        '''
+
+        X = self.X
+        sigma = self.kernel.sigma
+        N = self.ndata
+        D = self.ndim
+
+        K= self.kernel.gram_matrix(Y)
+        K_exp = tf.expand_dims(K, -1)
+
+        S = tf.expand_dims(X,1) - tf.expand_dims(Y,0)
+
+        b =  -D * tf.reduce_sum(K,1)
+        b += 2/sigma * tf.reduce_sum( K * tf.reduce_sum(S**2, 2), 1)
+
+        # term inside the square that multiplies alpha_i
+        A   = S * K_exp
+        SA  = tf.einsum('ikl,jkl->ij', A, A)
+        C   = (SA)
+
+        return b, C
+
+
+    def lite_fit(self, data=None, lam = 0.01):
+
+        npoint = self.npoint
+        Y = self._process_data(data)
+        b, C = self._lite_score_stats(Y)
+       
+        alpha_hat =  -self.kernel.sigma/2.0 * \
+                        tf.matrix_solve( C, tf.expand_dims(b,-1) )
+        self.kernel.alpha = tf.assign(self.kernel.alpha, tf.squeeze(alpha_hat))
+      
+        return self.kernel.alpha
+
+
+
+        
+        
+
 class Network:
 
     def __init__(self, ndata, ndim_in, ndim):
--- test_LiteNet.py
+++ test_LiteNet.py
@@ -39,33 +39,60 @@ class test_score(unittest.TestCase):
         kernel = ExponentialKernel()
 
         # fake data and points
-        self.data = tf.constant(np.random.randn(self.ndata,self.ndim_in))
-        self.points = tf.constant(np.random.randn(self.npoint,self.ndim_in))
-
+        self.data =   tf.constant(np.random.randn(self.ndata, self.ndim_in).astype('float32'))
+        self.points = tf.constant(np.random.randn(self.npoint, self.ndim_in).astype('float32'))
+        
+        # build network
         network = Network(self.ndata, self.ndim_in, self.ndim)
+
+        # setup lite model with fake alpha and points
         self.model = LiteModel(kernel, network)
+        self.model.kernel.alpha = tf.Variable( np.random.randn(self.npoint).astype('float32') )
+        self.model.set_points(self.points)
+        
+        # compute and store the features 
+        self.Y = self.model.network.forward(self.data)
+        self.X = self.model.kernel.X
+        self.model.X = self.model.kernel.X
+        init = tf.global_variables_initializer()
+
+        self.sess = tf.InteractiveSession()
+        self.sess.run(init)
 
     def test_score(self):
 
         # fake coefficients
-        self.model.alpha = tf.constant(np.random.randn(self.npoint))
-        self.model.set_points(self.points)
         score = self.model.score(self.data)
-        init = tf.global_variables_initializer()
-        with tf.Session() as sess:
-            sess.run(init)
-            sess.run(score)
-            alpha = self.model.alpha.eval()
-            X = self.model.kernel.X.eval()
-            Y = self.model.network.forward(self.data.eval())
-            true_score = 0.0
-            sigma = self.model.kernel.sigma
-            for l in range(self.ndim):
-                for i in range(self.npoint):
-                    for j in range(self.ndata):
-                        true_score += alpha[i]*np.exp(-np.linalg.norm(X[i]-Y[j])**2/sigma)* \
-                            (-1+2.0/sigma*(X[i,l]-Y[j,l])**2)
-            true_score *=  2 / self.ndata/sigma
-            assert np.allclose(score.eval(), true_score)
+
+        alpha = self.model.kernel.alpha.eval()
+        X = self.X.eval()
+        Y = self.Y.eval()
+        true_score = 0.0
+        sigma = self.model.kernel.sigma
+        for l in range(self.ndim):
+            for i in range(self.npoint):
+                for j in range(self.ndata):
+                    true_score += alpha[i]*np.exp(-np.linalg.norm(X[i]-Y[j])**2/sigma) * (-1 + 2/sigma * (X[i,l]-Y[j,l])**2)
+        
+        for l in range(self.ndim):
+            for i in range(self.ndata):
+                sq = 0
+                for j in range(self.npoint):
+                    sq += alpha[j]*(X[j,l]-Y[i,l]) * np.exp(-np.linalg.norm(X[j]-Y[i])**2/sigma) 
+                true_score += sq**2.0/sigma
+        true_score *=  2 / self.ndata/sigma
+        assert np.allclose(score.eval(), true_score)
+
+    def test_lite_fit(self):
+        
+        scores = []
+        for i in range(10):
+            self.model.kernel.alpha = tf.assign(self.model.kernel.alpha, np.random.randn(self.npoint))
+            scores.append( self.model.score(self.data).eval() )
+
+        self.model.lite_fit(self.data, lam=0.0)
+        score_opt = self.model.score(self.data).eval()
+
+        assert np.all(score_opt<=scores)
 
 unittest.main()
