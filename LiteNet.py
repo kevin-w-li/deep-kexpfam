594eb165743bb61b9d4fa2a4a0aa6848515a29ad
Kevin Li <kevinwli@outlook.com>
2017-07-20 00:45:57 +0100

    derivative w.r.t parameter working

--- LiteNet.py
+++ LiteNet.py
@@ -1,9 +1,16 @@
 import tensorflow as tf
 import numpy as np
+from collections import OrderedDict
+import operator
+import itertools
 
-class ExponentialKernel:
+class GaussianKernel:
 
-    def __init__(self, sigma = 1.0, X = None):
+    def __init__(self, sigma = 1.0, X = None, alpha = None):
+
+        '''
+        X: points that define the kernel, X.shape = (npoint, ndim)
+        '''
             
         if X is not None:
             self.X = X
@@ -13,43 +20,110 @@ class ExponentialKernel:
             self.X  = None
             self.npoint = None
             self.ndim = None
+        if alpha is not None: 
+            assert alpha.shape[0] == X.shape[0]
+            self.alpha  = alpha
+        else:
+            self.alpha  = None
         self.sigma = 2.0
         self.pdist2 = None
+        self.grad   = None
+        self.hess   = None
 
-    def set_points(self, X):
+    def set_points(self, X, alpha = None):
         
         self.X = X
         self.npoint = X.shape[0].value
         self.ndim = X.shape[1].value
+        if alpha is not None:
+            assert alpha.shape[0] == X.shape[0]
+            self.alpha = alpha
 
-    def get_pdist2(self, Y, X=None):
-
-        if X is None:
-            X = self.X
+    def get_pdist2(self, Y):
+        
+        X = self.X
         pdist2 = tf.reduce_sum(X**2, axis=1, keep_dims=True)
         pdist2 -= 2.0*tf.matmul(X, Y, transpose_b = True)
         pdist2 += tf.matrix_transpose(tf.reduce_sum(Y**2, axis=1, keep_dims=True))
+        self.pdist2 = pdist2
         return pdist2
 
-    def gram_matrix(self,Y, X = None, sigma = None):
+    def set_pdist2(self, Y):
+
+        self.get_pdist2(Y)
         
-        if X is None:
-            X = self.X
-        self.pdist2 = self.get_pdist2(Y)
+    def get_gram_matrix(self,Y=None):
+        
+        if Y is not None:
+            self.pdist2 = self.get_pdist2(Y)
+        else:
+            if self.pdist2 is None:
+                self.set_pdist2(Y)
         pdist2 = self.pdist2
-        sigma = self.sigma if sigma == None else sigma
+        sigma = self.sigma
+        gram = tf.exp(-0.5/sigma*pdist2)
+        return gram
 
-        return tf.exp(-1.0/sigma*pdist2)
+    def evaluate_fun(self, Y):
+        '''
+        takes in input vector Y of shape (ninput x ndim) and return the 
+        function defined by the lite model, linear combination of kernel
+        functions 
+        
+        sum_m alpha_m * k(x_m, y_i)
+        
+        '''
+        if Y.shape.ndims == 1:
+            Y = tf.expand_dims(Y,0)
+        
+        gram = self.get_gram_matrix(Y)
 
+        return tf.einsum('i,ij', self.alpha, gram)
+        
+    def get_grad(self, Y):
 
-class LiteModel:
+        ''' first derivative of the function of lite model '''
+
+        X = self.X
+        gram = self.get_gram_matrix(Y)[:,:,None]
+
+        # D contrains the vector difference between pairs of x_m and y_i
+        # divided by sigma
+        D = (tf.expand_dims(X, 1) - tf.expand_dims(Y, 0))/self.sigma
+
+        # summing the first axes, 1 means the first 1 axes...
+        grad = tf.tensordot(self.alpha, gram * D, 1)
+
+        return grad
+    
+    def get_hess(self, Y):
+        '''
+        gram matrix, multiplied by ( outer products of x_m - y_i, minus
+                                     identity matrix of shape ndim x ndim)
+        '''
+        X = self.X
+        gram = self.get_gram_matrix(Y)[:,:,None, None]
+        # the first term
+        D = ( tf.expand_dims(X, 1) - tf.expand_dims(Y, 0) )/self.sigma
+        D2 = tf.einsum('ijk,ijl->ijkl', D, D)
+        I  = tf.eye( D.shape[-1].value )/self.sigma
+        I_exp = tf.expand_dims(tf.expand_dims(I, 0), 0)
+        hess  = tf.tensordot(self.alpha, gram * (D2 - I), 1)
+
+        return hess
+        
+        
+
+            
+
+        
+class KernelNetModel:
     
     def __init__(self, kernel, network):
         
         self.npoint = kernel.npoint
         self.ndim   = kernel.ndim
         self.ndim_in= network.ndim_in
-        self.ndata  =  network.ndata
         self.network = network
         self.kernel  = kernel
         self.X       = self.kernel.X
@@ -59,7 +133,7 @@ class LiteModel:
         Input is a set of images that will first be processed by network
         These are stored inside the model as parameters
         '''
-        self.kernel.set_points(self.network.forward(X))
+        self.kernel.set_points(self.network.forward_tensor(X))
         self.ndim = self.kernel.ndim
         self.npoint = self.kernel.npoint
         self.X      = self.kernel.X
@@ -70,18 +144,18 @@ class LiteModel:
             base measure q_0
         '''
 
-        sigma = self.kernel.sigma
-        N = self.ndata
+        sigma = self.kernel.sigma*1
+        N = data.shape[0].value
         alpha = self.kernel.alpha
         ndim  = self.ndim
 
         # output of the network, same as x's
-        Y = self.network.forward(data)
+        Y = self.network.forward_tensor(data)
 
         # compute gram matrix of network output with points
-        K = self.kernel.gram_matrix(Y)
-        D = 2.0/sigma*self.kernel.pdist2 - ndim
-        t = tf.reduce_sum(tf.expand_dims(alpha,1) * K * D) * 2.0 / N / sigma
+        K = self.kernel.get_gram_matrix(Y)
+        D = 1.0/sigma*self.kernel.pdist2 - ndim
+        J = tf.reduce_sum(tf.expand_dims(alpha,1) * K * D) * 1.0 / N / sigma
         
         # X_il
         # Y_jl
@@ -95,7 +169,7 @@ class LiteModel:
         # K_exp = K[:,:,None], K_ij.
         K_exp     = tf.expand_dims(K, -1)
 
-        J += 2.0/N/(sigma**2)*tf.reduce_sum(
+        J += 0.5/N/(sigma**2)*tf.reduce_sum(
                             tf.reduce_sum(alpha_exp * (S) * K_exp, 0
                         )**2)
 
@@ -104,16 +178,17 @@ class LiteModel:
     def _process_data(self, data):
 
         if data is not None:
-                Y = self.network.forward(data)
+                Y = self.network.forward_tensor(data)
         else:
             if self.X is None:
                 raise NameError('run set_point first to set the kernel points, or provide it as input')
             Y = self.X
         return Y
 
-    def score(self, data=None):
+    def kernel_score(self, data=None):
         
         Y = self._process_data(data)
+        ndata = data.shape.dims[0].value
 
         # alpha as vectors
         # alpha as vectors
@@ -123,14 +198,14 @@ class LiteModel:
 
         sigma = self.kernel.sigma
 
-        b, C = self._lite_score_stats(Y)
+        b, C = self._kernel_score_stats(Y)
 
-        J = 2.0 / self.ndata / sigma    * tf.einsum('i,i'   , alpha, b) + \
-            2.0 / self.ndata / sigma**2 * tf.einsum('i,ij,j', alpha, C, alpha)
+        J = 1.0 / ndata / sigma    * tf.einsum('i,i'   , alpha, b) + \
+            0.5 / ndata / sigma**2 * tf.einsum('i,ij,j', alpha, C, alpha)
 
         return J
 
-    def _lite_score_stats(self, Y):
+    def _kernel_score_stats(self, Y):
         
         ''' compute the vector b and matrix C
             Y: the input data to the lite model to fit
@@ -138,16 +213,16 @@ class LiteModel:
 
         X = self.X
         sigma = self.kernel.sigma
-        N = self.ndata
+        N = Y.shape[0]
         D = self.ndim
 
-        K= self.kernel.gram_matrix(Y)
+        K= self.kernel.get_gram_matrix(Y)
         K_exp = tf.expand_dims(K, -1)
 
         S = tf.expand_dims(X,1) - tf.expand_dims(Y,0)
 
         b =  -D * tf.reduce_sum(K,1)
-        b += 2/sigma * tf.reduce_sum( K * tf.reduce_sum(S**2, 2), 1)
+        b += 1/sigma * tf.reduce_sum( K * tf.reduce_sum(S**2, 2), 1)
 
         # term inside the square that multiplies alpha_i
         A   = S * K_exp
@@ -157,37 +232,239 @@ class LiteModel:
         return b, C
 
 
-    def lite_fit(self, data=None, lam = 0.01):
+    def kernel_fit(self, data=None, lam = 0.01):
 
         npoint = self.npoint
         Y = self._process_data(data)
-        b, C = self._lite_score_stats(Y)
+        b, C = self._kernel_score_stats(Y)
        
-        alpha_hat =  -self.kernel.sigma/2.0 * \
+        alpha_hat =  -self.kernel.sigma * \
                         tf.matrix_solve( C, tf.expand_dims(b,-1) )
         self.kernel.alpha = tf.assign(self.kernel.alpha, tf.squeeze(alpha_hat))
       
         return self.kernel.alpha
 
 
-
-        
+    def kernel_grad(self, data):
+        Y = self.net.forward_tensor(data)
+        return self.kernel.grad(Y)
         
 
 class Network:
+    
+    ndim_in = None
+    ndim_out= None
+    batch_size   = None
+    param   = None
+    out     = None
 
-    def __init__(self, ndata, ndim_in, ndim):
+    def __init__(self, ndim_in, ndim):
+
+        raise NotImplementedError('should implement individual networks')
+
+    def reshape_data_array(self, x):
+        if x.ndim == len(self.ndim_in):
+            if x.shape == self.ndim_in:
+                x = x[None,:]
+                single = True
+            else:
+                raise NameError('input dimension is wrong')
+        else:
+            if x.shape[1:] != self.ndim_in:
+                raise NameError('input dimension is wrong')
+            else:
+                single = False
+        return x, single
+
+    def reshape_data_tensor(self, x):
+        if x.shape.ndims == len(self.ndim_in):
+            if x.shape == self.ndim_in:
+                x = tf.expand_dims(x, 0)
+                single = True
+            else:
+                raise NameError('input dimension is wrong')
+        else:
+            if x.shape[1:] != self.ndim_in:
+                raise NameError('input dimension is wrong')
+            else:
+                single = False
+        return x, single
         
-        self.ndata = ndata
-        self.ndim  = ndim
+
+    def forward_array(self, data):
+        raise NotImplementedError('should implement individual networks')
+
+    def forward_tensor(self, data):
+        raise NotImplementedError('should implement individual networks')
+
+    def get_grad_param(self, data):
+
+        ''' Compute the derivative of each output to all the parameters
+        data: numpy array
+
+        '''
+
+        # reshape data to see if there is a single input
+        data, single   = self.reshape_data_array(data)
+        ninput = data.shape[0]
+        # number of batches
+        nbatch = ninput/self.batch_size
+
+        # parameters are duplicated (not copied in memory) to facilitate parallel gradient over input batch
+        # it creates different symbols that points to the same parameters via the tf.identity function
+        # each copy of the parameter will be paired with a single input data for forward computation
+        param_copy = [OrderedDict(
+                        zip( self.param.keys(), 
+                             [tf.identity(v) for v in self.param.values()]
+                           )
+                    ) for _ in xrange(self.batch_size)]
+       
+        # rearrange the parameter handles into a single array so tf.gradients can consume. It is arrange like:
+        #         [ w_1_copy1, w_1_copy_2, w_1_copy_3, ..., w_nparam_copy_batch_size ]
+        batch_param = [ param_copy[i][k] for k in self.param.keys() for i in xrange(self.batch_size) ]
+
+        # create input placeholder that has batch_size
+        one_batch = tf.placeholder(tf.float32, shape = (self.batch_size,) + self.ndim_in)
+
+        # split it into different symbols to be paired with parameter copies
+        one_batch_split = tf.split(one_batch, self.batch_size, axis = 0)
+    
+        # compute output of each data which is paired with a copy of parameter dictionary
+        output = [ self.forward_tensor(one_data[0],param=p) 
+                            for (one_data, p) in zip(one_batch_split,  param_copy) ]
+
+        # Create dummy variable so that gradient can be computed. This has the same length
+        # as the number of outputs.
+        # It works because the following
+        # Let the output be y_i(x_j), each data x_j is paired with parameter copy w_j,
+        # The i'th value of this variable, denoted by sum_i, is then sum_j y_i(x_j)
+        # Then d(sum_i) / d(w_j) = d(y_i(x_j))
+        # sum_output = [ sum_j y_0(x_j), sum_j y_1(x_j), sum_j y_2(x_j), .. , sum_j y_ndim_out(x_j) ]
+        sum_output = tf.reduce_sum(output, axis=0)
+
+        # Essentailly create ndim_out number of graphs that share parameters, each element is the dummpy
+        # variable that sums each output over input data batch
+        # grad will be arranged by [ number of output [len(batch_param)] ]
+        grad   = [ list(tf.gradients(sum_output[oi], batch_param)) for oi in xrange(self.ndim_out)]
+
+        # After evaluating the gradient, arrange the gradients into a dictionary so that copies of parameters
+        # are indexed by the same parameter name. This is done by looking for the gradient w.r.t. the correct 
+        # chunk in each tf.gradient output where parameters of the same name reside as in batch_param
+        grad_dict = OrderedDict.fromkeys(self.param.keys())
+        for ki, k in enumerate(self.param.keys()):
+            # look for the correct chunk in each output dimension
+            grad_k = [grad[oi][ki*self.batch_size:(ki+1)*self.batch_size] for oi in xrange(self.ndim_out)]
+            # flatten(grad_k)
+            grad_dict[k] = list(itertools.chain.from_iterable(grad_k))
+
+        # results stores grad over multiple batches
+        results = []
+        sess = tf.Session()
+        init = tf.global_variables_initializer()
+        sess.run(init)
+
+        for bi in xrange(nbatch):
+            # get a batch
+            batch_data = data[bi*self.batch_size:(bi+1)*self.batch_size]
+            # run and append results, the first argument is [[w_1_copies], [w_2_copies], network output]
+            results.append(sess.run(grad_dict.values() + [output], feed_dict = {one_batch : batch_data}))
+        
+        # create output dictionary what will be indexed by parameter name.
+        # grad_value_dict[parameter_name][output_idx][input_idx][param_dims]
+        grad_value_dict = OrderedDict.fromkeys(self.param.keys())
+       
+        # results are arranged as follows
+        # results[batch_idx][param][w_copyies that corresponds to each input data within batch]
+        # first fix a k, then form an array of [ndim_out x batch_size*nbatch]
+        for ki, k in enumerate(self.param.keys()):
+            grad_k = np.array([      
+                        np.concatenate([
+                            results[bi][ki][oi*self.batch_size:(oi+1)*self.batch_size] for bi in xrange(nbatch)
+                        ]) for oi in xrange(self.ndim_out)
+                     ])
+            grad_value_dict[k] = grad_k
+        
+        # extract the output by using the last index of each batch
+        output_value = np.concatenate([results[bi][-1] for bi in xrange(nbatch)])
+        return grad_value_dict, output_value
+
+    def get_grad_param_one_data(self, data):
+
+        if data.shape.ndims == 1 and data.shape[0] == self.ndim_in:
+            data = tf.expand_dims(data, 0)
+        return get_grad_param(data)
+
+
+    def get_sec_grad_data(self, data):
+
+        raise NotImplementedError('should implement individual networks')
+
+class LinearNetwork(Network):
+
+    def __init__(self, ndim_in, ndim_out, batch_size = 2):
+        
+        self.ndim_out  = ndim_out
         self.ndim_in = ndim_in
+        self.batch_size = batch_size
+        W     = tf.Variable(np.random.randn(ndim_out, *ndim_in).astype('float32'))
+        b      = tf.Variable(np.random.randn(1, ndim_out).astype('float32'))
+        self.param = OrderedDict([('W', W), ('b', b)])
+        self.out   = None
 
+    def forward_array(self, data, param = None):
+        
+        if param is None:
+            param = self.param
+        data, single = self.reshape_data_array(data)
+            
+        data_tensor  = tf.placeholder('float32', shape= (None, ) + self.ndim_in)
 
-    def forward(self, data):
-        return data
+        W = param['W']
+        b = param['b']
+        out = tf.matmul(data_tensor, W,  transpose_b = True)
+        out += b
+        sess = tf.Session()
+        init = tf.global_variables_initializer()
+        sess.run(init)
+        out_value = sess.run(out, feed_dict={data_tensor: data})
+        if single:
+            out_value = out_value[0]
+        return out_value
 
+    def forward_tensor(self, data, param = None):
+        
+        if param is None:
+            param = self.param
+        data, single = self.reshape_data_tensor(data)
+            
+        W = param['W']
+        b = param['b']
+        out = tf.matmul(data, W,  transpose_b = True)
 
+        out += b
 
+        if single:
+            out = out[0]
+        return out
+    def get_grad_param_one_out(self, data):
 
         
+        out = self.out
+
+        grad = lambda oi: zip(*[tf.gradients(out[di, oi], [W, b]) for di in range(ninput)])
+
+        grad_dict = lambda i: OrderedDict( zip(self.param.keys(), grad(i)) )
+
+        return grad_dict
+
+    def get_sec_grad(self, data):
+
+        if self.out is None:
+            self.forward_tensor(data)
         
+        out = self.out
+        ninput = out.shape.as_list()[0]
+        grad = tf.stack([tf.gradients(out[i], [W])[0] for i in np.arange(ninput)])
+        return grad
+
+
--- test_LiteNet.py
+++ test_LiteNet.py
@@ -3,55 +3,138 @@ from LiteNet import *
 import unittest
 import numpy as np
 
-
 class test_LiteNet(unittest.TestCase):
 
 
     def setUp(self):
-        self.kernel = ExponentialKernel()
-        self.X = tf.constant(np.random.randn(3,4))
-        self.Y = tf.constant(np.random.randn(2,4))
-        self.kernel.set_points(self.X)
+        self.kernel = GaussianKernel()
+        self.X = tf.constant(np.random.randn(3,4).astype('float32'))
+        self.Y = tf.constant(np.random.randn(2,4).astype('float32'))
+        self.alpha = tf.constant(np.random.randn(3).astype('float32'))
+        self.kernel.set_points(self.X, alpha = self.alpha)
 
-    def test_gram_matrix(self):
-        result = self.kernel.gram_matrix(self.Y)
         init = tf.global_variables_initializer()
-        with tf.Session() as sess:
-            sess.run(init)
-            x = self.X.eval()
-            y = self.Y.eval()
-            res = result.eval()
+        self.sess = tf.InteractiveSession()
+        self.sess.run(init)
 
-            true_res = np.array([[np.linalg.norm(x[i]-y[j])**2 for j in range(y.shape[0]) ]for i in range(x.shape[0])])
-            true_res = np.exp(-1.0/self.kernel.sigma*true_res)
-            assert np.allclose(res, true_res)
+    def test_get_gram_matrix(self):
+        result = self.kernel.get_gram_matrix(self.Y)
+        a = self.X.eval()
+        x = self.X.eval()
+        y = self.Y.eval()
+        res = result.eval()
+
+        true_res = np.array([[np.linalg.norm(x[i]-y[j])**2 for j in range(y.shape[0]) ]for i in range(x.shape[0])])
+        true_res = np.exp(-0.5/self.kernel.sigma*true_res)
+        assert np.allclose(res, true_res)
+
+    def test_evaluate_fun(self):
+        
+        self.kernel.set_pdist2(self.Y)
+        value = self.kernel.evaluate_fun(self.Y).eval()
+        gram = self.kernel.get_gram_matrix().eval()
+        alpha = self.alpha.eval()
+        assert value.shape[0] == self.Y.shape[0]
+        assert np.allclose(value, alpha.dot(gram))
+        
+
+    def test_get_grad(self):
+
+        grad = self.kernel.get_grad(self.Y).eval()
+        values = self.kernel.evaluate_fun(self.Y)
+        grad_real = np.empty(self.Y.shape)
+        
+        for vi in range(values.shape[0]):
+           grad_real[vi] = tf.stack(tf.gradients(values[vi], self.Y)[0][vi]).eval()
+           
+        assert np.allclose(grad, grad_real)
+
+    def test_get_hess(self):
+        
+        hess = self.kernel.get_hess(self.Y).eval()
+        ndim = self.kernel.ndim
+        ninput = self.Y.shape[0]
+
+        # hessian is of shape (ninput x ndim x ndim)
+        hess_real = np.empty( (ninput, ndim, ndim) )
+        for vi in range(ninput):
+            this_y = self.Y[vi]
+            values = self.kernel.evaluate_fun(this_y)
+            hess_real[vi] = tf.stack(tf.hessians(values[0], this_y)[0]).eval()
+        assert np.allclose(hess, hess_real)
+
+class test_LinearNetwork(unittest.TestCase):
+    
+    ndim_in = (5,)
+    ndim_out = 4
+    ndata  = 6
+
+    def setUp(self):
+        
+        # fake data and points
+        self.data =   np.random.randn(self.ndata, *self.ndim_in).astype('float32')
+        self.data_tensor = tf.constant(self.data)
+        
+        # build network
+        self.network = LinearNetwork(self.ndim_in, self.ndim_out)
+
+        self.sess = tf.InteractiveSession()
+
+        init = tf.global_variables_initializer()
+
+        self.sess.run(init)
 
 
-class test_score(unittest.TestCase):
+    def test_forward_array(self):
+        out = self.network.forward_array(self.data)
+        W = self.network.param['W'].eval()
+        b = self.network.param['b'].eval()
+        data = self.data
+        out_real = b + data.dot(W.T)
+        assert np.allclose(out, out_real)
+
+    def test_get_grad_param(self):
+        grad_dict, output = self.network.get_grad_param(self.data)
+        grad_dict_true = OrderedDict.fromkeys(self.network.param)
+        output_true    = self.network.forward_array(self.data)
+        for k in grad_dict_true:
+            grad_dict_true[k] = np.zeros([self.network.ndim_out, self.ndata] + self.network.param[k].shape.dims)
+
+        for di in xrange(self.ndata):
+            for oi in xrange(self.network.ndim_out):
+                grad_dict_true['W'][oi, di, oi] = self.data[di]
+                grad_dict_true['b'][oi, di, 0, oi] = 1
+
+        assert np.allclose(grad_dict_true['W'], grad_dict['W'])
+        assert np.allclose(grad_dict_true['b'], grad_dict['b'])
+        assert np.allclose(output_true, output)
+
+class test_kernel_score(unittest.TestCase):
 
     ndata  = 2
-    ndim_in = 5
-    ndim    = 5
+    ndim_in = (5,)
+    ndim    = 4
     npoint = 3
 
     def setUp(self):
         
-        kernel = ExponentialKernel()
+        kernel = GaussianKernel()
 
         # fake data and points
-        self.data =   tf.constant(np.random.randn(self.ndata, self.ndim_in).astype('float32'))
-        self.points = tf.constant(np.random.randn(self.npoint, self.ndim_in).astype('float32'))
+        self.data =   np.random.randn(self.ndata, *self.ndim_in).astype('float32')
+        self.data_tensor =   tf.constant(self.data)
+        self.points = tf.constant(np.random.randn(self.npoint, *self.ndim_in).astype('float32'))
         
         # build network
-        network = Network(self.ndata, self.ndim_in, self.ndim)
+        network = LinearNetwork(self.ndim_in, self.ndim)
 
         # setup lite model with fake alpha and points
-        self.model = LiteModel(kernel, network)
+        self.model = KernelNetModel(kernel, network)
         self.model.kernel.alpha = tf.Variable( np.random.randn(self.npoint).astype('float32') )
         self.model.set_points(self.points)
         
         # compute and store the features 
-        self.Y = self.model.network.forward(self.data)
+        self.Y = self.model.network.forward_array(self.data)
         self.X = self.model.kernel.X
         self.model.X = self.model.kernel.X
         init = tf.global_variables_initializer()
@@ -59,40 +142,47 @@ class test_score(unittest.TestCase):
         self.sess = tf.InteractiveSession()
         self.sess.run(init)
 
-    def test_score(self):
+    def test_original_score(self):
+
+        score = self.model.kernel_score(self.data_tensor)
+        score_o = self.model.score_original(self.data_tensor)
+        assert np.allclose(score.eval(), score_o.eval())
+
+    def test_kernel_score(self):
 
         # fake coefficients
-        score = self.model.score(self.data)
+        score = self.model.kernel_score(self.data_tensor)
 
         alpha = self.model.kernel.alpha.eval()
         X = self.X.eval()
-        Y = self.Y.eval()
+        Y = self.Y
         true_score = 0.0
         sigma = self.model.kernel.sigma
         for l in range(self.ndim):
             for i in range(self.npoint):
                 for j in range(self.ndata):
-                    true_score += alpha[i]*np.exp(-np.linalg.norm(X[i]-Y[j])**2/sigma) * (-1 + 2/sigma * (X[i,l]-Y[j,l])**2)
+                    true_score += alpha[i]*np.exp(-np.linalg.norm(X[i]-Y[j])**2/sigma/2.0) * (-1 + 1/sigma * (X[i,l]-Y[j,l])**2)
         
         for l in range(self.ndim):
             for i in range(self.ndata):
                 sq = 0
                 for j in range(self.npoint):
-                    sq += alpha[j]*(X[j,l]-Y[i,l]) * np.exp(-np.linalg.norm(X[j]-Y[i])**2/sigma) 
-                true_score += sq**2.0/sigma
-        true_score *=  2 / self.ndata/sigma
+                    sq += alpha[j]*(X[j,l]-Y[i,l]) * np.exp(-np.linalg.norm(X[j]-Y[i])**2/sigma/2.0) 
+                true_score += sq**2.0/(sigma)/2.0
+        true_score *=  1.0 / self.ndata/sigma
         assert np.allclose(score.eval(), true_score)
 
-    def test_lite_fit(self):
+    def test_kernel_fit(self):
         
         scores = []
         for i in range(10):
             self.model.kernel.alpha = tf.assign(self.model.kernel.alpha, np.random.randn(self.npoint))
-            scores.append( self.model.score(self.data).eval() )
+            scores.append( self.model.kernel_score(self.data_tensor).eval() )
 
-        self.model.lite_fit(self.data, lam=0.0)
-        score_opt = self.model.score(self.data).eval()
+        self.model.kernel_fit(self.data_tensor, lam=0.0)
+        score_opt = self.model.kernel_score(self.data_tensor).eval()
 
         assert np.all(score_opt<=scores)
+
 
 unittest.main()
