46d03d530f4ae92a07b995678fc5af2a865dad86
kevin-w-li <kevinwli@outlook.com>
2018-11-22 17:39:24 +0000

    Create setup.py
    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name='DKEF',
+      version='0.1',
+      description='Code for Deep Kernel Exponential Family',
+      url='https://github.com/kevin-w-li/deep-kexpfam',
+      author='Li Kevin Wenliang',
+      author_email='kevinwli@outlook.com',
+      license='BSD3',
+      zip_safe=False)
